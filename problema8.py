824017d5289c0904f7ad6eb9da7f2eb70bd436a6
Gerard Farràs i Ballabriga <gfarrasb@gmail.com>
2017-03-27 11:13:28 +0200

    Add files via upload

---
+++ problema8.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.5
+#
+# Solucio al problema 8 . HpCodeWars 2017.
+
+
+a=input()
+for x in a:
+	if x.isupper():
+		print(x.lower(),end="")
+	else:
+		print(x.upper(),end="")
+print()
+
+
+	
+
